e071a589e3967566f674af7d3a8452d2fb30624e
sandeboina srikanth <sandeboinasrikanth@gmail.com>
2024-05-25 15:14:17 +0530

    created for react notes

---
+++ src/Notes.js
@@ -0,0 +1,45 @@
+/*
+website: react.org , react.dev
+=> A javascript library for building  user interface.
+=> Reactjs was created by Facebook And it is open soure.
+=> Reactjs is single page Application.
+=> To know react before you have to understand the pre-requisites like: HTML, CSS, JAVASCRIPT.
+
+
+Tools :
+Chrome, vscode, nodejs/npm , git/github accounts 
+
+concepts like: chrome Dev tools, Npm, Linux/terminal shortcuts 
+
+
+Download Tools :
+chrome Download
+vs code download
+nodejs ->use LTS(long time support)-stable version
+git/github download
+
+check in terminals (or) Shortcut keys:
+node -v, node --version
+npm -v, npx -v
+git --version
+
+cls, clear
+pwd (present working directory)
+cd .. -> backward
+cd Desktop/hello -> forward
+cd (current directory)
+cd ~ home directory
+
+Browser Devtools:
+-> When we develop App we use it ,
+-> panels -> Element, Console, Lighthouse, Network
+-> React Developer Tools
+-> Redux Devtools
+
+
+
+
+
+
+
+*/
